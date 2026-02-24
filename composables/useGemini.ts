664afa492cd98241f6839309128a155b1d54c09e
Leo Pfeiffer <50983452+leo-pfeiffer@users.noreply.github.com>
2026-02-24 18:17:28 -0500

    Update useGemini.ts (#32)
    Update gemini model

--- composables/useGemini.ts
+++ composables/useGemini.ts
@@ -17,7 +17,7 @@ export const useGemini = () => {
     
     const classify = async(ingredients: string[]): Promise<String> => {
         const prompt = await makePrompt(ingredients);
-        const resp = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
+        const resp = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
