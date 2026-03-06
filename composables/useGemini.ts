28938edcbf09bf8c6c0edc1b4dc5206a5c613b9a
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2026-03-06 18:04:05 -0500

    Cleanup

--- composables/useGemini.ts
+++ composables/useGemini.ts
@@ -6,29 +6,28 @@ export const useGemini = () => {
     }
 
     const generate = async (prompt: string): Promise<string> => {
-        const resp = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`, {
+        return await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                "contents": [{ "parts": [{ "text": prompt }] }]
+                "contents": [{"parts": [{"text": prompt}]}]
             })
         })
-        .then(res => res.json())
-        .then(data => {
-            try {
-                return data.candidates[0].content.parts[0].text;
-            } catch (error) {
-                console.error(error);
-                throw Error("Error parsing response from Gemini.");
-            }
-        })
-        .catch(err => {
-            console.error(err);
-            throw Error("Error fetching from Gemini.");
-        });
-        return resp;
+            .then(res => res.json())
+            .then(data => {
+                try {
+                    return data.candidates[0].content.parts[0].text;
+                } catch (error) {
+                    console.error(error);
+                    throw Error("Error parsing response from Gemini.");
+                }
+            })
+            .catch(err => {
+                console.error(err);
+                throw Error("Error fetching from Gemini.");
+            });
     }
 
     return { generate };
